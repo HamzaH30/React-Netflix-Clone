8dcf90123e9f4dba3dc12b5a16f63d71f00c0688
Hamza Haque <hamzahaque30@gmail.com>
2023-06-09 19:05:59 -0500

    Test the API to see that it is working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const apiKey = process.env.REACT_APP_API_KEY;
+const apiURL = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}`;
 
 function App() {
   return <div className="App"></div>;
