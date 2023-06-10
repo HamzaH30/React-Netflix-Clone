bf1b56ab542232f88ce4dbff31607d619d4c561e
Hamza Haque <hamzahaque30@gmail.com>
2023-06-10 16:16:46 -0500

    Pagination mostly done. Need to look at TODOs

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,36 +1,43 @@
 import { useEffect, useState } from "react";
 import TitleList from "./TitleList";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import Pagination from "./pagination-related-part/Pagination";
 
 export default function Search(props) {
   const [showsData, setShowsData] = useState([]);
   const [searchParams] = useSearchParams();
   const showQuery = searchParams.get("show");
-  const navigate = useNavigate();
 
   // Pagination
   const [totalPages, setTotalPages] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
-  const pageQuery = searchParams.get("page") ?? 1;
+  const [currentPage, setCurrentPage] = useState(searchParams.get("page") ?? 1);
+  const [invalidPageEntered, setInvalidPageEntered] = useState(false);
 
   // Update the search data shown whenever the user enters in a new search
   useEffect(() => {
     const fetchShowData = async () => {
-      const response = await fetch(
-        `${props.baseURL}search/tv?api_key=${
-          props.apiKey
-        }&query=${showQuery}&page=${+pageQuery}`
-      );
-      const data = await response.json();
-      const totalPages = data["total_pages"];
-      setTotalPages(totalPages);
-      setCurrentPage(pageQuery);
-      setShowsData(data.results);
+      if (currentPage > 0) {
+        const response = await fetch(
+          `${props.baseURL}search/tv?api_key=${
+            props.apiKey
+          }&query=${showQuery}&page=${+currentPage}`
+        );
+        const data = await response.json();
+        const totalPages = data["total_pages"];
+        setTotalPages(totalPages);
+
+        if (currentPage > totalPages) {
+          setInvalidPageEntered(true);
+        } else {
+          setShowsData(data.results);
+        }
+      } else {
+        setInvalidPageEntered(true);
+      }
     };
 
     fetchShowData();
-  }, [showQuery, pageQuery]);
+  }, [showQuery, currentPage]);
 
   return (
     <>
@@ -40,7 +47,14 @@ export default function Search(props) {
         handleWatchListToggle={props.handleWatchListToggle}
         watchList={props.watchList}
       />
-      <Pagination totalPages={totalPages} currentPage={+currentPage} />
+      <Pagination
+        totalPages={totalPages}
+        currentPage={+currentPage}
+        setCurrentPage={setCurrentPage}
+        showQuery={showQuery}
+        searchParams={searchParams}
+        invalidPageEntered={invalidPageEntered}
+      />
     </>
   );
 }
--- src/components/pagination-related-part/Pagination.jsx
+++ src/components/pagination-related-part/Pagination.jsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
 import "./pagination.css";
+import { useNavigate } from "react-router-dom";
+
+// TODO: NEED TO ASK ABOUT CSS MODULES!! Not exactly sure if I have met that requirement
 
 export default function Pagination(props) {
   const [pages, setPages] = useState([]);
+  const navigate = useNavigate();
+
   useEffect(() => {
     const pages = [];
     for (let i = 1; i <= props.totalPages; i++) {
@@ -11,11 +16,67 @@ export default function Pagination(props) {
     setPages(pages);
   }, [props.totalPages]);
 
+  function handleNavigationClick(pageOffset) {
+    const newPage = props.currentPage + pageOffset;
+    props.searchParams.set("page", newPage);
+    props.setCurrentPage(newPage);
+
+    // Show change in URL
+    navigate(`/search?show=${props.showQuery}&page=${newPage}`);
+  }
+
   return (
-    <div className="pagination-container">
-      {pages.map((page, index) => {
-        return <button key={index}>{page}</button>;
-      })}
-    </div>
+    <>
+      {props.invalidPageEntered ? (
+        <>
+          <p>Invalid Page Entered!</p>
+          {props.totalPages === 1 ? (
+            <p>There is only 1 page. Please only enter page 1.</p>
+          ) : (
+            <p>Pages must be between 1 - {props.totalPages}.</p>
+          )}
+        </>
+      ) : (
+        <div className="pagination-container">
+          {props.totalPages > 1 && props.currentPage > 1 && (
+            <button
+              className="navigate-pages"
+              onClick={() => handleNavigationClick(-1)}
+            >
+              Previous Page
+            </button>
+          )}
+          {props.totalPages > 1 && (
+            <div className="buttons">
+              {pages.map((page, index) => {
+                return (
+                  <button
+                    className="page-btn"
+                    onClick={() => {
+                      props.setCurrentPage(page);
+                      props.searchParams.set("page", page);
+                      navigate(`/search?show=${props.showQuery}&page=${page}`);
+                    }}
+                    key={index}
+                  >
+                    {page}
+                  </button>
+                );
+              })}
+            </div>
+          )}
+          {props.currentPage < props.totalPages ? (
+            <button
+              className="navigate-pages"
+              onClick={() => handleNavigationClick(1)}
+            >
+              Next Page
+            </button>
+          ) : (
+            <p>End of Results!</p>
+          )}
+        </div>
+      )}
+    </>
   );
 }
