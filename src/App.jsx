468d8411ad1e8cfae174c70913ed296a4afb8e58
Hamza Haque <hamzahaque30@gmail.com>
2023-06-10 02:47:25 -0500

    Install and Import Font Awesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./styles/reset.css";
 import "./styles/style.css";
+import "font-awesome/css/font-awesome.min.css";
 import Main from "./components/Main";
 import Header from "./components/Header";
 import Search from "./components/Search";
